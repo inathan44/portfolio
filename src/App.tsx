835bdc0bca1f1c7ac924814838f474832905ca9c
inathan44 <inathan44@yahoo.com>
2023-05-22 01:17:44 -0400

    Added hero section to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar/Index';
 
 function App() {
   return (
     <div className='font-inter'>
       <Navbar />
-      <div></div>
+      <Hero />
     </div>
   );
 }
